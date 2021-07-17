1904b6a42e9ba46c411d55d8a0fcd3e8ed230b52
cscazorla <cscazorla@gmail.com>
2021-07-17 16:21:38 +0200

    Added coments to the RayCast function and updated the README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Raycasting
-Implementing a basic Raycasting renderer in C similiar to the [Wolfenstein 3D](https://github.com/id-Software/wolf3d) technique (angles and trigonometry). As I'm using expensive functions like sine, cosine and tangent, this is not the fastest raycasting implementation.
+Implementing a basic Raycasting renderer in C similiar to the [Wolfenstein 3D](https://github.com/id-Software/wolf3d) technique. 
 
 ![Demo](./assets/demo.gif)
 
@@ -10,6 +10,7 @@ Assumptions in my implementation:
 * Floor is flat
 * Walls are made of cubes with the same size
 
-# Compilation
+As I'm using angles to represent orientation, I require expensive functions like sine, cosine and tangent. Therefore, this is not the fastest raycasting implementation. If you pursue performance, you shoud look into the famous [Lodev article](lodev.org/cgtutor/raycasting.html), that uses vectors (x,y) to represent orientation.
 
+# Compilation
 This project is built in C99 and it compiles perfectly with GCC. The project uses [SDL](https://www.libsdl.org/) to deal with pixels, keyboard, etc.
--- src/ray.c
+++ src/ray.c
@@ -13,10 +13,10 @@ struct Ray {
     float wallHitY;
     float distance;
     int wasHitVertical;
-    int isRayFacingUp;
-    int isRayFacingDown;
-    int isRayFacingLeft;
-    int isRayFacingRight;
+    int isFacingUp;
+    int isFacingDown;
+    int isFacingLeft;
+    int isFacingRight;
     int wallHitContent;
 } rays[NUM_RAYS];
 
@@ -24,7 +24,12 @@ struct Ray {
  * Function: castRay
  * -------------------
  * Cast a ray from a specific coordinate (x,y) and angle. Once the
- * ray hits a wall, it stores information in the rays array
+ * ray hits a wall, it stores information in the rays array.
+ * 
+ * DDA algorithm:
+ * To be more efficient, instead of checking every single pixel of
+ * the ray, we just check the horizontal and vertical intersections
+ * of the mapa grid (every TILE_SIZE) and confirm whether it is a wall.
  * 
  * float rayAngle: angle of the ray from the player position
  * float x: Horizontal coordinate
@@ -36,104 +41,102 @@ struct Ray {
 void castRay(float rayAngle, float x, float y, int stripId) {
     rayAngle = normalizeAngle(rayAngle);
     
-    int isRayFacingDown = rayAngle > 0 && rayAngle < PI;
-    int isRayFacingUp = !isRayFacingDown;
-
-    int isRayFacingRight = rayAngle < 0.5 * PI || rayAngle > 1.5 * PI;
-    int isRayFacingLeft = !isRayFacingRight;
+    // Which direction is the player facing?
+    int isFacingDown = rayAngle > 0 && rayAngle < PI;
+    int isFacingUp = !isFacingDown;
+    int isFacingRight = rayAngle < 0.5 * PI || rayAngle > 1.5 * PI;
+    int isFacingLeft = !isFacingRight;
     
-    float xintercept, yintercept;
+    // Intersections are separated evenly
     float xstep, ystep;
 
-    ///////////////////////////////////////////
-    // HORIZONTAL RAY-GRID INTERSECTION CODE
-    ///////////////////////////////////////////
+    // We check for horizontal and vertical intersections
+    // separately. The distance to both horizontal
+    // and vertical intersection points is compared
+    // (we select the closest one)
+
+    /***********************************************
+    * Horizontal intersections check
+    ************************************************/
     int foundHorzWallHit = false;
     float horzWallHitX = 0;
     float horzWallHitY = 0;
     int horzWallContent = 0;
 
-    // Find the y-coordinate of the closest horizontal grid intersection
+    // Find the y-coordinate and x-coordinate of the closest horizontal grid intersection
+    float xintercept, yintercept;
     yintercept = floor(y / TILE_SIZE) * TILE_SIZE;
-    yintercept += isRayFacingDown ? TILE_SIZE : 0;
-
-    // Find the x-coordinate of the closest horizontal grid intersection
+    yintercept += isFacingDown ? TILE_SIZE : 0;
     xintercept = x + (yintercept - y) / tan(rayAngle);
 
-    // Calculate the increment xstep and ystep
+    // xstep and ystep calculation
     ystep = TILE_SIZE;
-    ystep *= isRayFacingUp ? -1 : 1;
+    ystep *= isFacingUp ? -1 : 1;
+    xstep = ystep / tan(rayAngle);
+    xstep *= (isFacingLeft && xstep > 0) ? -1 : 1;
+    xstep *= (isFacingRight && xstep < 0) ? -1 : 1;
 
-    xstep = TILE_SIZE / tan(rayAngle);
-    xstep *= (isRayFacingLeft && xstep > 0) ? -1 : 1;
-    xstep *= (isRayFacingRight && xstep < 0) ? -1 : 1;
-
-    float nextHorzTouchX = xintercept;
-    float nextHorzTouchY = yintercept;
-
-    // Increment xstep and ystep until we find a wall
-    while (nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT) {
-        float xToCheck = nextHorzTouchX;
-        float yToCheck = nextHorzTouchY + (isRayFacingUp ? -1 : 0);
+    // Main loop to find an horizontal wall hit
+    float horizontalIntersectionNextX = xintercept;
+    float horizontalIntersectionNextY = yintercept;
+    while (horizontalIntersectionNextX >= 0 && horizontalIntersectionNextX <= WINDOW_WIDTH && horizontalIntersectionNextY >= 0 && horizontalIntersectionNextY <= WINDOW_HEIGHT) {
+        float xToCheck = horizontalIntersectionNextX;
+        float yToCheck = horizontalIntersectionNextY + (isFacingUp ? -1 : 0);
         
         if (mapHasWallAt(xToCheck, yToCheck)) {
             // found a wall hit
-            horzWallHitX = nextHorzTouchX;
-            horzWallHitY = nextHorzTouchY;
+            horzWallHitX = horizontalIntersectionNextX;
+            horzWallHitY = horizontalIntersectionNextY;
             horzWallContent = getMapTileContent(xToCheck, yToCheck);
             foundHorzWallHit = true;
             break;
         } else {
-            nextHorzTouchX += xstep;
-            nextHorzTouchY += ystep;
+            horizontalIntersectionNextX += xstep;
+            horizontalIntersectionNextY += ystep;
         }
     }
-    
-    ///////////////////////////////////////////
-    // VERTICAL RAY-GRID INTERSECTION CODE
-    ///////////////////////////////////////////
+
+    /***********************************************
+    * Vertical intersections check
+    ************************************************/
     int foundVertWallHit = false;
     float vertWallHitX = 0;
     float vertWallHitY = 0;
     int vertWallContent = 0;
 
-    // Find the x-coordinate of the closest horizontal grid intersection
+    // Find the y-coordinate and x-coordinate of the closest vertical grid intersection
     xintercept = floor(x / TILE_SIZE) * TILE_SIZE;
-    xintercept += isRayFacingRight ? TILE_SIZE : 0;
-
-    // Find the y-coordinate of the closest horizontal grid intersection
+    xintercept += isFacingRight ? TILE_SIZE : 0;
     yintercept = y + (xintercept - x) * tan(rayAngle);
 
-    // Calculate the increment xstep and ystep
+    // xstep and ystep calculation
     xstep = TILE_SIZE;
-    xstep *= isRayFacingLeft ? -1 : 1;
+    xstep *= isFacingLeft ? -1 : 1;
+    ystep = xstep * tan(rayAngle);
+    ystep *= (isFacingUp && ystep > 0) ? -1 : 1;
+    ystep *= (isFacingDown && ystep < 0) ? -1 : 1;
 
-    ystep = TILE_SIZE * tan(rayAngle);
-    ystep *= (isRayFacingUp && ystep > 0) ? -1 : 1;
-    ystep *= (isRayFacingDown && ystep < 0) ? -1 : 1;
-
-    float nextVertTouchX = xintercept;
-    float nextVertTouchY = yintercept;
-
-    // Increment xstep and ystep until we find a wall
-    while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
-        float xToCheck = nextVertTouchX + (isRayFacingLeft ? -1 : 0);
-        float yToCheck = nextVertTouchY;
+    // Main loop to find an horizontal wall hit
+    float verticalIntersectionNextX = xintercept;
+    float verticalIntersectionNextY = yintercept;
+    while (verticalIntersectionNextX >= 0 && verticalIntersectionNextX <= WINDOW_WIDTH && verticalIntersectionNextY >= 0 && verticalIntersectionNextY <= WINDOW_HEIGHT) {
+        float xToCheck = verticalIntersectionNextX + (isFacingLeft ? -1 : 0);
+        float yToCheck = verticalIntersectionNextY;
         
         if (mapHasWallAt(xToCheck, yToCheck)) {
             // found a wall hit
-            vertWallHitX = nextVertTouchX;
-            vertWallHitY = nextVertTouchY;
+            vertWallHitX = verticalIntersectionNextX;
+            vertWallHitY = verticalIntersectionNextY;
             vertWallContent = getMapTileContent(xToCheck, yToCheck);
             foundVertWallHit = true;
             break;
         } else {
-            nextVertTouchX += xstep;
-            nextVertTouchY += ystep;
+            verticalIntersectionNextX += xstep;
+            verticalIntersectionNextY += ystep;
         }
     }
 
-    // Calculate both horizontal and vertical hit distances and choose the smallest one
+    // Which distance (vertical or horizontal) is the smallest?
     float horzHitDistance = foundHorzWallHit
         ? distanceBetweenPoints(x, y, horzWallHitX, horzWallHitY)
         : FLT_MAX;
@@ -141,6 +144,7 @@ void castRay(float rayAngle, float x, float y, int stripId) {
         ? distanceBetweenPoints(x, y, vertWallHitX, vertWallHitY)
         : FLT_MAX;
 
+    // Information about the hit (e.g. coordinates or map content) is stored in the array of rays
     if (vertHitDistance < horzHitDistance) {
         rays[stripId].distance = vertHitDistance;
         rays[stripId].wallHitX = vertWallHitX;
@@ -155,10 +159,10 @@ void castRay(float rayAngle, float x, float y, int stripId) {
         rays[stripId].wasHitVertical = false;
     }
     rays[stripId].rayAngle = rayAngle;
-    rays[stripId].isRayFacingDown = isRayFacingDown;
-    rays[stripId].isRayFacingUp = isRayFacingUp;
-    rays[stripId].isRayFacingLeft = isRayFacingLeft;
-    rays[stripId].isRayFacingRight = isRayFacingRight;
+    rays[stripId].isFacingDown = isFacingDown;
+    rays[stripId].isFacingUp = isFacingUp;
+    rays[stripId].isFacingLeft = isFacingLeft;
+    rays[stripId].isFacingRight = isFacingRight;
 }
 
 /*
